8b85adf49313fd287a4a7da22fc008be128286c1
Richard5038 <master48630756@gmail.com>
2024-04-08 04:19:48 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32{
+    if 0 <= num && num <= 40{
+        num * 2
+    }else{
+        num
+    }//else{
+        //println!("Wrong input!");
+        //break;
+    //}
+}
 
 // Don't modify this function!
 #[test]
